c7b19bca32e97f65c02a7cbb327650a6918b0478
Silas Santos Leite <estoudente.program@gmail.com>
2021-04-28 11:38:32 -0300

    VERSION 1.0 / Netflify

--- src/app/beer.service.ts
+++ src/app/beer.service.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs";
   providedIn: 'root'
 })
 export class BeerService {
-  private baseUrl = 'http://localhost:8080/api/v1/beers';
+  private baseUrl = 'https://beer-stock.herokuapp.com/api/v1/beers';
 
   constructor(private http: HttpClient) { }
 
